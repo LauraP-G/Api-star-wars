f521c74b25cae7de813cc754b1b6e84a81be990f
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-07-19 17:40:29 +0000

    añadidas varias tablas y todos los endpoints para user

--- src/app.py
+++ src/app.py
@@ -36,14 +36,77 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users=User.query.all()
+    users=[user.serialize() for user in users]
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    if not users:
+        return jsonify({'msg':'No hay usuarios'}),200
+    return jsonify ({'data': users}),200
+    #Tambien podemos pasar la respues del body así:
+    # response_body ={
+    #     'Users': users
+    # }
+    # return jsonify(response_body),200
 
-    return jsonify(response_body), 200
+@app.route('/add_user', methods=['POST'])
+def add_usser():
+    data=request.json
+    if data['email'] and data['password']:
+        user=User.query.filter_by(email=data['email']).first()
+        if user:
+            return jsonify ({'msg':'Parece que ya te conocemos, intenta logearte'}),200
+        new_user=User(email=data['email'], password=data['password'], is_active=True)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'msg': 'Usuario creado', 'user': new_user.serializa()}),200
+    
+    return jsonify({'msg':'todos los datos son necesarios'}),400
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user(id):
+    user= User.query.get(id)
+    if not user:
+        return jsonify({'msg': '¡Oh no! No encontramos tu cuenta'}),404
+    return jsonify({'msg': 'mostrando usuario', 'user': user.serialize()}),200
+
+@app.route('/edit_user/<int:id>', methods=['PUT'])
+def edit_user(id):
+    data=request.json
+    if data['email'] and data['is_active']:
+        user=User.query.get(id)
+        #user.email esto es el atributo de la clase User
+        #data['email'] valor del diccionario data
+        # es un diccionario obtenido del cuerpo de la solicitud JSON (request.json). 
+        # data['email'] accede al valor asociado con la clave 'email' en este diccionario.
+        user.email=data['email'] or user.email
+        user.is_active=data['is_active'] or user.is_active
+        db.session.commit()
+        if not user:
+            return jsonify({'msg':'usuario no encontrado'})
+        return jsonify({'msg':'usuario editado', 'user': user.serialize()}),200
+    return jsonify({'error': 'Todos los datos son necesarios'})
+
+
+@app.route('/delete_user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+     user= User.query.get(id)
+     if user:
+         db.session.delete(user)
+         db.session.commit()
+         return jsonify({'msg':'usuario eliminado'}),204
+     return jsonify({'msg':'no se encontró usuario a eliminar'}), 404
+
+    
+    
+
+
+
+
+
+
+   
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,80 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    favourite=db.relationship('Favourites', back_populetes="user")
+
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<User {self.email}>' 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            'is_active':self.is_active
             # do not serialize the password, its a security breach
         }
+    
+class Favourites(db.Model):
+    __tablename__ = 'favourites'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    
+    user = db.relationship('User', back_populates='favourite')
+    character = db.relatioship('Characters', back_populates='favourite')
+     
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "user_id": self.user_id
+            
+        }
+    
+class Characters(db.Model):
+     __tablename__ = 'starships'
+
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(250), nullable=False)
+     model = db.Column(db.String(150))
+     starship_class = db.Column(db.String(150))
+     manufacturer =  db.Column(db.String(150))
+     cost_in_credits = db.Column(db.Integer)
+     length = db.Column(db.Integer)
+     crew = db.Column(db.Integer)
+     passengers = db.Column(db.Integer)
+     max_atmosphering_speed = db.Column(db.Integer)
+     cargo_capacity = db.Column(db.Integer)
+
+     favourite = db.relationship('Favourites', back_populates='charater')
+
+     def serialize(self):
+         return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "starship_class": self.starship_class,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "cargo_capacity": self.cargo_capacity
+        }
+       
+         
+      
+    
+    
+
